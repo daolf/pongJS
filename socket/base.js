082721c858868bbf257a700eb451f156d49a4dc8
Pierre De Wulf <pierredewulf31@gmail.com>
2015-02-16 20:02:35 +0100

    ADD commucation c/s touche fail

--- socket/base.js
+++ socket/base.js
@@ -1,5 +1,5 @@
 module.exports = function (io) {
     io.on('keyPressed', function (socket) {
-      console.log("La touche appuyée est : " + socket.keyCode);
+      console.log("La touche appuyée est : ");
     });
 };
