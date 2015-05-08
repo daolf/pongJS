e447f9d9fb070e8e8b2ecc6420f5cc8764bd5222
Pierre De Wulf <pierredewulf31@gmail.com>
2015-05-08 14:23:02 +0200

    complete refactoring of collision

--- socketServeur/racket.js
+++ socketServeur/racket.js
@@ -20,11 +20,24 @@ Racket.prototype.moveDown = function(height) {
         this.info[1] += this.info[4];
 };
 
-Racket.prototype.isCollide = function(objX, objY, objW, objH) {
-    if (objX < this.info[0] + this.info[2] && objX + objW > this.info[0] &&
-        objY < this.info[1] + this.info[3] && objY + objH > this.info[1]) 
-    {
+/*
+  Return true if the given point is in the racket, else false
+*/
+Racket.prototype.hasPointIn = function(pointX,pointY) {
+  var isIn = false;
+  if (pointY >= this.info[1] &&
+      pointY <= this.info[1] + this.info[3] &&
+      pointX >= this.info[0] &&
+      pointX <= this.info[0] + this.info[2]
+      ){ isIn = true; }
+  return isIn;
+};
 
+Racket.prototype.isCollide = function(objX, objY, objW, objH) {
+    // We test if all point of given object are in the racket
+    if (  this.hasPointIn(objX,objY) || this.hasPointIn(objX + objW,objY) ||
+          this.hasPointIn(objX,objY+objH) || this.hasPointIn(objX+objW,objY+objH))
+    {
         console.log("Collision détectée !");
         return (true);
     }
